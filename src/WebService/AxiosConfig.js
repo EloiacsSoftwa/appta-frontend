9911f5e6a775cf7c9e00bd316f55cc86136c0847
Mathubala <premathujasvi5706@gmail.com>
2024-10-30 17:58:20 +0530

    update code

--- src/WebService/AxiosConfig.js
+++ src/WebService/AxiosConfig.js
@@ -1,12 +1,33 @@
 import axios from 'axios'
+import Cookies from 'universal-cookie';
+import config from './Config';
+
+
+
+
+const cookies = new Cookies();
+
 
 const AxiosConfig = axios.create({
-    baseURL: "http://localhost:2001",
+    baseURL: config.apiBaseUrl,
 headers: {
   'Content-Type': 'application/json',
     'Access-Control-Allow-Origin' : '*'
 }
-
 })
-
+AxiosConfig.interceptors.request.use(
+  (config) => {
+      const token = cookies.get('token');
+      console.log("token Bearer",token)
+      if (token) {
+          config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+  },
+  (error) => {
+      return Promise.reject(error);
+      
+  }
+);
 export default AxiosConfig;
+
