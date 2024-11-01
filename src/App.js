5b0d3c663ed27a5df41292478f064fd4888ab12d
arputha <arputha2000apm@gmail.com>
2024-11-01 12:36:47 +0530

    add product

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
 import 'tailwindcss/tailwind.css';
 import First from './Components/First';
-
+import ProductList from './Product Pages/ProductList';
 
 function App() {
   return (
     <div className="App">
-    <First/>
+    <ProductList/>
     </div>
   );
 }
