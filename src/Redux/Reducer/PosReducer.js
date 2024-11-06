96169574fe41c1b059c53fe823eb95194f4b37e9
Allwin <allwinpon@gmail.com>
2024-11-06 10:05:37 +0530

    pos correction complted

--- src/Redux/Reducer/PosReducer.js
+++ src/Redux/Reducer/PosReducer.js
@@ -1,24 +1,42 @@
 const initialState = {
-    BarcodeproductData: [],
-    barcodeStatuscode: 0,
-   
-  };
+  BarcodeproductData: [],
+  barcodeStatuscode: 0,
+  Errormsg: ''
+};
+
+const PosReducer = (state = initialState, action) => {
+  console.log("action",action);
   
-  const PosReducer = (state = initialState, action) => {
-    switch (action.type) {
-        
-      case 'BARCODE-GET-PRODUCT':
-        return {...state, BarcodeproductData:action.payload.response,  barcodeStatuscode: action.payload.statusCode};
-                     
-      case 'REMOVE_GET_BARCODE_PRODUCT_STATUS_CODE':
-       return{...state, barcodeStatuscode:0 }
- 
- 
-    
-      default:
-        return state;
-    }
-  };
-  
-  export default PosReducer;
-  
+  switch (action.type) {
+    // case 'BARCODE_GET_PRODUCT_SUCCESS':
+    //   return {
+    //     ...state,
+    //     BarcodeproductData: [...state.PosReducer.BarcodeproductData, action.payload.data],
+    //     barcodeStatuscode: action.payload.statusCode,
+    //   };
+
+    case 'BARCODE_GET_PRODUCT_SUCCESS':
+      return {
+        ...state,
+        BarcodeproductData:  action.payload.data,
+        barcodeStatuscode: action.payload.statusCode,
+      };
+
+    case 'BARCODE_GET_PRODUCT_FAILURE':
+      return {
+        ...state,
+        Errormsg: action.payload.message,
+      };
+
+    case 'REMOVE_GET_BARCODE_PRODUCT_STATUS_CODE':
+      return { ...state, barcodeStatuscode: 0 };
+
+    case 'CLEAR_BARCODE_GET_PRODUCT_FAILURE':
+      return { ...state, Errormsg: '' };
+
+    default:
+      return state;
+  }
+};
+
+export default PosReducer;
--- src/Redux/Saga_Middleware/Pos_Saga.js
+++ src/Redux/Saga_Middleware/Pos_Saga.js
@@ -2,42 +2,38 @@ import { call, takeEvery, put } from 'redux-saga/effects';
 import { PosGetbyBarcode } from '../Action/sales_pos_Action';
 import Cookies from 'universal-cookie';
 
-
-
 function* handleBarcodeGetProduct(args) {
 
     const response = yield call(PosGetbyBarcode, args.payload);
-   console.log("Response For pos ",response)
-    if (response.status === 200 || response.statusCode === 200) {
-      const token = response.data;
-      yield put({ type: 'BARCODE-GET-PRODUCT', payload: {response:response.data.data , statusCode: response.status  || response.statusCode}});
-            
-    }
-    else{
-        yield put({ type: 'ERROR', payload: {response:response.data.message , statusCode: response.status  || response.statusCode}});
-
-    }
-
+  
+    console.log("response",response);
     
+    if (response && (response.status === 200 || response.code === 200)) {
+        yield put({ 
+          type: 'BARCODE_GET_PRODUCT_SUCCESS', 
+          payload: { data: response.data, statusCode: response.status || response.code } 
+        });
+    } else {
+        yield put({ 
+          type: 'BARCODE_GET_PRODUCT_FAILURE', 
+          payload: { message: response ? response.message : "Unable to fetch product data", statusCode: response?.status || 500 } 
+        });
+    }
+
     if (response) {
-      ExpireToken(response)
-   }
-
-  } 
-
-
-  function ExpireToken(response) {
-   if (response.data.code === 204) {
-     const message = response.data.code
-     const cookies = new Cookies()
-     cookies.set('access-denied', message, { path: '/' });
-   }
-
+        ExpireToken(response);
+    }
 }
 
+function ExpireToken(response) {
+    if (response.data.code === 204) {
+        const cookies = new Cookies();
+        cookies.set('access-denied', response.data.code, { path: '/' });
+    }
+}
 
-  function* PosSaga() {
-  yield takeEvery('BARCODE_GET_PRODUCT', handleBarcodeGetProduct);
-   }
+function* PosSaga() {
+    yield takeEvery('BARCODE_GET_PRODUCT', handleBarcodeGetProduct);
+}
 
 export default PosSaga;
