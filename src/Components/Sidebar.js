d37ff4b9f1a99b4accf3645f2e391f6f89ec6636
Mathubala <premathujasvi5706@gmail.com>
2024-11-08 10:41:37 +0530

    update

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -87,7 +87,7 @@ useEffect(() => {
     items.map((item) => (
       <li
         key={item}
-        className="flex items-center gap-8 text-xs font-normal font-manrope"
+        className="flex items-center gap-8 text-xs font-normal font-manrope "
         onClick={() => handleSelectedMenu(item)}
       >
         
@@ -105,7 +105,7 @@ useEffect(() => {
     <li key={title} className="">
       <div
         onClick={() => toggleSubmenu(title)}
-        className="flex items-center cursor-pointer text-white pl-4 hover:bg-gray-700 rounded-md"
+        className="flex items-center cursor-pointer text-white pl-4 pt-1.5 pb-1.5 hover:bg-gray-700 rounded-md"
       >
         <img src={icon} alt={`${title} Icon`} className="w-5 h-5" />
         {isExpanded && <span className="ml-6 text-base font-normal font-manrope">{title}</span>}
